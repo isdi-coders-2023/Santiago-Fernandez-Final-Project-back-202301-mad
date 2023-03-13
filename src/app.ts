749a2161d787bf721419152568c69f234ab93f44
sfernandez <sfdezlop@gmail.com>
2023-03-13 22:54:19 +0100

    Add index and app

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import path from 'path';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { usersRouter } from './routers/users.router.js';
+import createDebug from 'debug';
+import { CustomError } from './interfaces/error.js';
+import { __dirname } from './config.js';
+const debug = createDebug('ERP:app');
+export const app = express();
+app.disable('x-powered-by');
+
+const corsOptions = {
+  origin: '*',
+};
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors(corsOptions));
+
+debug({ __dirname });
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+
+app.get('/', (_req, resp) => {
+  resp.json({
+    info: 'Santiago-Fernandez-Final-Project-back-202301-mad',
+    endpoints: {
+      users: '/users',
+    },
+  });
+});
+
+app.use(
+  (error: CustomError, _req: Request, resp: Response, _next: NextFunction) => {
+    debug('Middleware de errores');
+    const status = error.statusCode || 500;
+    const statusMessage = error.statusMessage || 'Internal server error';
+    resp.status(status);
+    resp.json({
+      error: [
+        {
+          status,
+          statusMessage,
+        },
+      ],
+    });
+    debug(status, statusMessage, error.message);
+  }
+);
