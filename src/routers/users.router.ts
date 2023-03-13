f8c479c27ffc112063ad3fc093bc2ef826214134
sfernandez <sfdezlop@gmail.com>
2023-03-13 22:57:12 +0100

    Add router and controller

---
+++ src/routers/users.router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { UsersController } from '../controllers/users.controller.js';
+import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
+import createDebug from 'debug';
+const debug = createDebug('ERP:router:users');
+
+// eslint-disable-next-line new-cap
+export const usersRouter = Router();
+debug('loaded');
+
+const repo = UsersMongoRepo.getInstance();
+const controller = new UsersController(repo);
+
+usersRouter.post('/register', controller.register.bind(controller));
+usersRouter.post('/login', controller.login.bind(controller));
