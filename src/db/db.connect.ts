35d26ee16d930f62a9ef63cadb16e018b118708b
sfernandez <sfdezlop@gmail.com>
2023-03-14 22:45:18 +0100

    Bypass dbconnect

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -3,7 +3,14 @@ import { config } from '../config.js';
 
 const { user, passwd, cluster, dbName } = config;
 
-export const dbConnect = () => {
-  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+// Export const dbConnect = () => {
+//   const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+//   return mongoose.connect(uri);
+// };
+
+export const dbConnect = (env?: string) => {
+  const finalEnv = env || process.env.NODE_ENV;
+  const finalDBName = finalEnv === 'test' ? dbName + '_Testing' : dbName;
+  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
   return mongoose.connect(uri);
 };
