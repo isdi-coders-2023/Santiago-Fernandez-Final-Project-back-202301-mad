c2b815ebd0595ec183cbd8eef8e619bf2cf71fcf
sfernandez <sfdezlop@gmail.com>
2023-03-15 11:46:53 +0100

    Clean commented code

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -6,11 +6,6 @@ const debug = createDebug('ERP:dbConnect');
 
 const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME } = config;
 
-// Export const dbConnect = () => {
-//   const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${dbName}?retryWrites=true&w=majority`;
-//   return mongoose.connect(uri);
-// };
-
 export const dbConnect = (env?: string) => {
   const finalEnv = env || process.env.NODE_ENV;
   const finalDBName = finalEnv === 'test' ? DB_NAME + '_Testing' : DB_NAME;
