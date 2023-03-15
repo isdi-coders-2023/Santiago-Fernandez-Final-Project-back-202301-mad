4990fbd3680fd7d8ce252a384ce040dfbae375d3
sfernandez <sfdezlop@gmail.com>
2023-03-15 11:00:33 +0100

    Add case to test

--- src/db/db.connect.test.ts
+++ src/db/db.connect.test.ts
@@ -4,6 +4,14 @@ import { dbConnect } from './db.connect';
 describe('Given a dbConnect to mongo atlas function', () => {
   describe('When it is invoked', () => {
     test('Then should connect with the production database', async () => {
+      const result = await dbConnect();
+      expect(result).toBe(mongoose);
+      expect(mongoose.connection.db.databaseName).toContain('ERP');
+      mongoose.disconnect();
+    });
+  });
+  describe('When it is invoked', () => {
+    test('Then should connect with the production database', async () => {
       const result = await dbConnect('env');
       expect(result).toBe(mongoose);
       expect(mongoose.connection.db.databaseName).toContain('ERP');
--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 import { config } from '../config.js';
+import createDebug from 'debug';
+
+const debug = createDebug('ERP:dbConnect');
 
 const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME } = config;
 
@@ -12,5 +15,6 @@ export const dbConnect = (env?: string) => {
   const finalEnv = env || process.env.NODE_ENV;
   const finalDBName = finalEnv === 'test' ? DB_NAME + '_Testing' : DB_NAME;
   const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${finalDBName}?retryWrites=true&w=majority`;
+  debug(uri);
   return mongoose.connect(uri);
 };
