fcc950d171711d36180ee75b1e60851cc0e39f91
sfernandez <sfdezlop@gmail.com>
2023-03-18 10:02:32 +0100

    Add interceptor logged

---
+++ src/interceptors/logged.test.ts
@@ -0,0 +1,51 @@
+import { Response } from 'express';
+import { User } from '../entities/user.entity';
+import { Repo } from '../repositories/repo.interface';
+import { logged, RequestPlus } from '../interceptors/logged';
+import { Auth } from '../services/auth';
+import { HTTPError } from '../interfaces/error';
+
+jest.mock('../services/auth');
+
+describe('Given AuthInterceptor class', () => {
+  const repo: Repo<User> = {
+    create: jest.fn(),
+    search: jest.fn(),
+    countRecords: jest.fn(),
+  };
+  // Const interceptor = new AuthInterceptor(repo);
+
+  const req = {
+    body: {},
+    params: { id: '' },
+    get: jest.fn(),
+  } as unknown as RequestPlus;
+  const resp = {
+    json: jest.fn(),
+  } as unknown as Response;
+  const next = jest.fn();
+
+  describe('When logged is used', () => {
+    test('Then it should send next if there are NOT Authorization header ', () => {
+      (req.get as jest.Mock).mockReturnValue(null);
+      logged(req, resp, next);
+      expect(next).toHaveBeenLastCalledWith(expect.any(HTTPError));
+    });
+    test('Then it should send next if Authorization header is BAD formatted', () => {
+      (req.get as jest.Mock).mockReturnValue('BAD token');
+      logged(req, resp, next);
+      expect(next).toHaveBeenLastCalledWith(expect.any(HTTPError));
+    });
+    test('Then it should send next if Authorization token is NOT valid', () => {
+      Auth.verifyJWTGettingPayload = jest.fn().mockReturnValue('Invalid token');
+      logged(req, resp, next);
+      expect(next).toHaveBeenLastCalledWith(expect.any(Error));
+    });
+    test('Then it should send next if Authorization token is valid', () => {
+      (req.get as jest.Mock).mockReturnValue('Bearer token');
+      Auth.verifyJWTGettingPayload = jest.fn().mockReturnValue({});
+      logged(req, resp, next);
+      expect(next).toHaveBeenLastCalledWith();
+    });
+  });
+});
