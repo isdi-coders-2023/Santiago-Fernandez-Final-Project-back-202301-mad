24b9fe22c95b222cb8cb94345e59e5ac65c4245c
sfernandez <sfdezlop@gmail.com>
2023-04-06 20:57:03 +0200

    Delete debugs

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -10,6 +10,6 @@ export const dbConnect = (env?: string) => {
   const finalEnv = env || process.env.NODE_ENV;
   const finalDBName = finalEnv === 'test' ? DB_NAME + '_Testing' : DB_NAME;
   const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${finalDBName}?retryWrites=true&w=majority`;
-  debug(uri);
+  // Debug(uri);
   return mongoose.connect(uri);
 };
